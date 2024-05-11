5583654d7158897e72c752e98fc0ee9e9f309352
Jason Cheng <jasoncky96@gmail.com>
2024-05-11 17:53:37 +0800

    feature: added update and delete reminder functions

--- pkg/schemas/reminder.go
+++ pkg/schemas/reminder.go
@@ -22,12 +22,67 @@ type Reminder struct {
 	InConstruction bool   `json:"in_construction"`
 }
 
-// func getReminderBy
+// func getReminder
 
 func (reminder Reminder) Create() error {
 	endpoint := fmt.Sprintf("%v/items/reminder", core.DirectusHost)
 	reqBody, _ := json.Marshal(reminder)
 	req, httpErr := http.NewRequest(http.MethodPost, endpoint, bytes.NewBuffer(reqBody))
+	req.Header.Set("Content-Type", "application/json")
+	if httpErr != nil {
+		return httpErr
+	}
+	client := &http.Client{}
+	res, httpErr := client.Do(req)
+	if httpErr != nil {
+		return httpErr
+	}
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+	if res.StatusCode != 200 {
+		return fmt.Errorf("error inserting reminder to directus: %v", res.Status)
+	}
+	var reminderResponse map[string]Reminder
+	jsonErr := json.Unmarshal(body, &reminderResponse)
+	// error handling for json unmarshaling
+	if jsonErr != nil {
+		return jsonErr
+	}
+
+	return nil
+}
+
+func (reminder Reminder) Update() error {
+	endpoint := fmt.Sprintf("%v/items/reminder/%v", core.DirectusHost, reminder.Id)
+	reqBody, _ := json.Marshal(reminder)
+	req, httpErr := http.NewRequest(http.MethodPatch, endpoint, bytes.NewBuffer(reqBody))
+	req.Header.Set("Content-Type", "application/json")
+	if httpErr != nil {
+		return httpErr
+	}
+	client := &http.Client{}
+	res, httpErr := client.Do(req)
+	if httpErr != nil {
+		return httpErr
+	}
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+	if res.StatusCode != 200 {
+		return fmt.Errorf("error inserting reminder to directus: %v", res.Status)
+	}
+	var reminderResponse map[string]Reminder
+	jsonErr := json.Unmarshal(body, &reminderResponse)
+	// error handling for json unmarshaling
+	if jsonErr != nil {
+		return jsonErr
+	}
+
+	return nil
+}
+
+func (reminder Reminder) Delete() error {
+	endpoint := fmt.Sprintf("%v/items/reminder/%v", core.DirectusHost, reminder.Id)
+	req, httpErr := http.NewRequest(http.MethodDelete, endpoint, nil)
 	req.Header.Set("Content-Type", "application/json")
 	if httpErr != nil {
 		return httpErr
