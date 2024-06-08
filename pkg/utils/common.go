8128e472d4f55a015eaed9b428a7e58564ab55e0
Jason Cheng <jasoncky96@gmail.com>
2024-06-08 14:53:21 +0800

    edited support message

--- pkg/utils/common.go
+++ pkg/utils/common.go
@@ -14,8 +14,7 @@ const HELP_MESSAGE string = `This bot lets you set reminders! The following comm
 
 
 Note that all reminders set on this bot can be accessed by the user hosting this bot. Do not set any reminders that contain any sort of private information.`
-const SUPPORT_MESSAGE string = `My source code is hosted on https://github.com/Jason-CKY/telegram-reminderbot. Consider 
-Post any issues with this bot on the github link, and feel free to contribute to the source code with a pull request.`
+const SUPPORT_MESSAGE string = `My source code is hosted on https://github.com/Jason-CKY/telegram-reminderbot. Post any issues with this bot on the github link, and feel free to contribute to the source code with a pull request.`
 const DEFAULT_SETTINGS_MESSAGE = "The default timezone for the bot is Asia/Singapore (GMT +8). Type /settings for more information on how to change the timezone. "
 const REMINDER_BUILDER_MESSAGE string = `Please enter reminder text. This bot allows for image reminders as well. Just attach an image and put your reminder text as the caption.`
 const CANCEL_MESSAGE string = `🚫 Cancel`
